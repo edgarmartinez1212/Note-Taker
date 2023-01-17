03d9eb4e6f903edaa59bc3068c1f3e085c712561
Edgar Martinez <edgarmartinez@Edgars-MBP.attlocal.net>
2023-01-16 21:27:15 -0600

    created routes file, index.js and notes.js files, created get and post routes for notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const api = require("./routes");
 
 const PORT = process.env.port || 3001;
 
@@ -8,9 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/api", api);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.listen(PORT, () => {
